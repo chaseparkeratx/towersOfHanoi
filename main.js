f78c079f123b3c16bb47ab3ca314407fe7eebe9b
chaseparkeratx <112991010+chaseparkeratx@users.noreply.github.com>
2023-04-30 12:59:06 -0500

    finished project

--- main.js
+++ main.js
@@ -7,14 +7,6 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-// An object that represents the three stacks of Towers of Hanoi; 
-  // * each key is an array of Numbers: 
-    // * A is the far-left, 
-    // * B is the middle, 
-    // * C is the far-right stack
-      // * Each number represents the largest to smallest tokens: 
-        // * 4 is the largest, 
-        // * 1 is the smallest
 
 let stacks = {
   a: [4, 3, 2, 1],
@@ -22,33 +14,43 @@ let stacks = {
   c: []
 };
 
-// Start here. What is this function doing?
+
 const printStacks = () => {
   console.log("a: " + stacks.a);
   console.log("b: " + stacks.b);
   console.log("c: " + stacks.c);
 }
 
-// Next, what do you think this function should do?
+
+
 const movePiece = (startStack, endStack) => {
-  let piece = stacks[startStack].pop()
-  stacks[endStack].push(piece)
+  // take the last part of the array, make it a 'piece'
+  // push it to next stack
+const startPiece = stacks[startStack].pop();
+stacks[endStack].push(startPiece);
+return true;
 }
 
-// Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
-const isLegal = () => {
-}
+const isLegal = (startStack, endStack) => {
+  // take the last piece from the first and second choice 
+  // if the second stack is empty or a larger number than the first, return true
+   const startPiece = stacks[startStack][stacks[startStack].length - 1];
+   const endPiece = stacks[endStack][stacks[endStack].length - 1];
+    return !endPiece || startPiece < endPiece 
+};
 
-// What is a win in Towers of Hanoi? When should this function run?
+
 const checkForWin = () => {
-  // Your code here
-
+  // if either stack b or c have a length of 4, return true
+  return stacks.b.length === 4 || stacks.c.length === 4;
 }
 
-// When is this function called? What should it do with its argument?
+
 const towersOfHanoi = (startStack, endStack) => {
+
   if(isLegal(startStack, endStack)) {
-    movePiece(startStack, endStack)
+    movePiece(startStack, endStack);
+    checkForWin();
   }
 }
 
